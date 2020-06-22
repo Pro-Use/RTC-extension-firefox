8e3a96a30513983b0fd5aa05df0744b8c127fd5e
pro-use <robertprouse@gmail.com>
2020-06-22 18:28:54 +0100

    info icon to extension icon

--- info/info.js
+++ info/info.js
@@ -1,6 +1,6 @@
 var info_div = document.createElement("div");
-var node = document.createTextNode("i");
-info_div.appendChild(node);
+//var node = document.createTextNode("i");
+//info_div.appendChild(node);
 info_div.id = "rtc_info_button";
 document.body.appendChild(info_div);
 
