fcf760fdf780dd9c6a4937a73199246d1a96017c
pro-use <robertprouse@gmail.com>
2020-07-07 12:22:49 +0100

    Fix Gretchen resizing

--- popups/gretchenandrew/js/resize.js
+++ popups/gretchenandrew/js/resize.js
@@ -1,6 +1,11 @@
-let img = document.getElementById('image');
-var frame_width = img.offsetWidth - window.innerWidth;
-var frame_height = img.offsetHeight - window.innerHeight;
-window.resizeBy(frame_width,frame_height);
-
-
+var img = document.getElementById('image');
+let container = document.getElementById('img_container');
+let src = container.dataset.src;
+console.log(src);
+container.onload = function() {
+    img.style.display = "block";
+    var frame_width = img.offsetWidth - window.innerWidth;
+    var frame_height = img.offsetHeight - window.innerHeight;
+    window.resizeBy(frame_width,frame_height);
+};
+container.src = src;
