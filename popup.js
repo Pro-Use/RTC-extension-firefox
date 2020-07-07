17636095da85dfabc59cbfc0df71b09e015da0ac
pro-use <robertprouse@gmail.com>
2020-07-07 15:46:59 +0100

    Add event text

--- popup.js
+++ popup.js
@@ -62,49 +62,86 @@ var title = document.getElementById('popup-work-title');
 var artist = document.getElementById('popup-artist');
 var time = document.getElementById('popup-time');
 
-var next_ts = null;
+//check for events:
+var d = new Date();
+var n = d.getTimezoneOffset() / 60;
 
-chrome.alarms.getAll(function (alarms) {
-    alarm_times = [];
-    alarms.forEach(function(alarm) {
-        if (alarm.name !== "countdown") {
-            alarm_times.push(alarm.scheduledTime);
+var pv = [
+    new Date(Date.UTC(2020, 23, 7, 18 + n, 30)),
+    new Date(Date.UTC(2020, 23, 7, 21 + n, 30))
+];
+console.log(pv);
+
+var talk = [
+    new Date(Date.UTC(2020, 6, 8, 19 + n, 00)),
+    new Date(Date.UTC(2020, 6, 8, 21 + n, 00))
+];
+console.log(talk);
+var now = d.getTime();
+
+console.log(pv[0].getTime() +"<" + now + "<" + pv[1].getTime());
+
+if (pv[0].getTime() < now && now < pv[1].getTime()) {
+    console.log("PV!");
+    title.textContent = "Real Time Constraints";
+    artist.textContent = "Opening";
+    time.textContent = pv[0];
+    document.getElementById("timer").style.display = "none";
+    document.getElementById("live").style.display = "block";
+    
+} else if (talk[0].getTime() < now && now < talk[1].getTime()) {
+    console.log("Talk!");
+    title.textContent = "Real Time Constraints";
+    artist.textContent = "Panel Discussion";
+    time.textContent = talk[0];
+    document.getElementById("timer").style.display = "none";
+    document.getElementById("live").style.display = "block";
+    
+} else {
+    var next_ts = null;
+
+    chrome.alarms.getAll(function (alarms) {
+        alarm_times = [];
+        alarms.forEach(function(alarm) {
+            if (alarm.name !== "countdown") {
+                alarm_times.push(alarm.scheduledTime);
+            }
+        });
+        alarm_times.sort(function(a, b){return a - b});
+        next_alarm_time = alarm_times[0];
+        for (i = 0; i < alarms.length; i++) {
+            alarm = alarms[i];
+            if (alarm.scheduledTime === next_alarm_time){
+                next_ts = alarm.scheduledTime;
+                alarm_time = new Date(next_ts);
+                info = work_info[alarm.name];
+                console.log(info);
+                artist.textContent = info[0];
+                title.textContent = info[1];
+                time.textContent = alarm_time;
+                break;
+            }        
         }
     });
-    alarm_times.sort(function(a, b){return a - b});
-    next_alarm_time = alarm_times[0];
-    for (i = 0; i < alarms.length; i++) {
-        alarm = alarms[i];
-        if (alarm.scheduledTime === next_alarm_time){
-            next_ts = alarm.scheduledTime;
-            alarm_time = new Date(next_ts);
-            info = work_info[alarm.name];
-            console.log(info);
-            artist.textContent = info[0];
-            title.textContent = info[1];
-            time.textContent = alarm_time;
-            break;
-        }        
-    }
-});
 
-var x = setInterval(function() {
-    // Get today's date and time
-    var now = new Date().getTime();
+    var x = setInterval(function() {
+        // Get today's date and time
+        var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = next_ts - now;
+        // Find the distance between now and the count down date
+        var distance = next_ts - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Display the result in the element with id="demo"
-    countdown_str = hours + "h " + minutes + "m " + seconds + "s ";
-    document.getElementById("timer").innerHTML = countdown_str;
+        // Time calculations for days, hours, minutes and seconds
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Display the result in the element with id="demo"
+        countdown_str = hours + "h " + minutes + "m " + seconds + "s ";
+        document.getElementById("timer").innerHTML = countdown_str;
 
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-    }
-}, 1000);
+        // If the count down is finished, write some text
+        if (distance < 0) {
+          clearInterval(x);
+        }
+    }, 1000);
+}
